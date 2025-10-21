1921438c10b65cbc6718088268e4804c9a6550d8
Gideon <gideon.w.jones@gmail.com>
2025-10-20 23:14:49 -0600

    Register Service and Results setup

---
+++ server/src/main/java/service/RegisterService.java
@@ -0,0 +1,36 @@
+package service;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+import java.util.UUID;
+
+public class RegisterService {
+    private final DataAccess dataAccess;
+
+    public RegisterService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public RegisterResult register(RegisterRequest request) throws DataAccessException {
+        if (request.username() == null || request.username().isEmpty() ||
+                request.password() == null || request.password().isEmpty()
+                || request.email() == null || request.email().isEmpty()) {
+            throw new DataAccessException("Error: Bad Request");
+        }
+
+        UserData existingUser = dataAccess.getUser(request.username());
+        if (existingUser != null) {
+            throw new DataAccessException("Error: User already exists");
+        }
+
+        UserData newUser = new UserData(request.username(), request.password(), request.email());
+        dataAccess.createUser(newUser);
+
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, request.username());
+        dataAccess.createAuth(authData);
+
+        return new RegisterResult(request.username(), authToken);
+    }
+}
