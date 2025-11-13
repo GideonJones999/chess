e5db96dfb75e1b212199988d7d7ab5b6d8a067ac
Gideon <gideon.w.jones@gmail.com>
2025-11-12 21:10:15 -0700

    Phase 5: Removed Server Module Reference from Client

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -7,16 +7,9 @@ public class Main {
     public static void main(String[] args) {
 //        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
 //        System.out.println("♕ 240 Chess Client: " + piece);
-        Server server = new Server();
-        int port = server.run(8080);
-        System.out.println("Server Started on port "+ port);
-        ServerFacade facade = new ServerFacade(port);
+        ServerFacade facade = new ServerFacade(8080);
 
         PreloginUI ui = new PreloginUI(facade);
         ui.run();
-
-        server.stop();
-
-
     }
 }
