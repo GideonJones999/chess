62f8a23aa4e89017e9362abbba5a671d43e1ff46
Gideon <gideon.w.jones@gmail.com>
2025-11-12 21:14:58 -0700

    Phase 5: Added Models to Server

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,12 +1,8 @@
-import chess.*;
-import server.Server;
 import serverfacade.ServerFacade;
 import ui.*;
 
 public class Main {
     public static void main(String[] args) {
-//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-//        System.out.println("♕ 240 Chess Client: " + piece);
         ServerFacade facade = new ServerFacade(8080);
 
         PreloginUI ui = new PreloginUI(facade);
